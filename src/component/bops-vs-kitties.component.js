0429b4227693dada847dd67e4232562bfa993166
Yonatan Kra <yonatan.kr@walkme.com>
2019-05-29 08:45:05 +0300

    Add a circle

--- src/component/bops-vs-kitties.component.js
+++ src/component/bops-vs-kitties.component.js
@@ -10,5 +10,19 @@ export class BopsVsKitties extends HTMLElement {
         super();
         this.root = this.attachShadow({mode: 'open'});
         this.root.appendChild(template.content.cloneNode(true));
+
+        this.init();
+    }
+
+    init() {
+        const canvas = this.root.querySelector('canvas');
+
+        const ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.arc(40, 60, 15, 0, Math.PI * 2); //x, y, radius, start angle, end angle
+        ctx.fillStyle = '#0095DD'; // just fill it
+        ctx.fill();
+        ctx.closePath();
+
     }
 }
