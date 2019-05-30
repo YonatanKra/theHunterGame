0265652bddbdc7e71381245a49b333ff886c15f8
Yonatan Kra <yonatan.kr@walkme.com>
2019-05-30 05:46:16 +0300

    Animation

--- src/component/bops-vs-kitties.component.js
+++ src/component/bops-vs-kitties.component.js
@@ -10,5 +10,32 @@ export class BopsVsKitties extends HTMLElement {
         super();
         this.root = this.attachShadow({mode: 'open'});
         this.root.appendChild(template.content.cloneNode(true));
+        this.canvas = this.root.querySelector('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.ballAnimation();
+    }
+    ballAnimation() {
+        let i = 0;
+        const interval = setInterval(() =>{
+            i++;
+            if (i > 300) {
+                window.clearInterval(interval);
+                return;
+            }
+            // clear the frame
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        // draw the ball
+        this.drawBall();
+        }, 16);
+    }
+    drawBall() {
+        this.x = this.x ? this.x + 1 : 1;
+        this.y = this.y ? this.y + 1 : 1;
+        // add a circle
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, 15, 0, Math.PI * 2); //x, y, radius, start angle, end angle
+        this.ctx.fillStyle = '#0095DD'; // just fill it
+        this.ctx.fill();
+        this.ctx.closePath();
     }
 }
